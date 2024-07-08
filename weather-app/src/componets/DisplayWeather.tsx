05929e92785feeeb95c2dc77dbfaa7a4a9322134
kimyoda <rladygks1210@naver.com>
2024-07-09 00:55:10 +0900

    [feat] Weatheropen Type of weather, isLoding setting, Search add, Eenter event add, Icon Ui add

--- weather-app/src/componets/DisplayWeather.tsx
+++ weather-app/src/componets/DisplayWeather.tsx
@@ -32,10 +32,14 @@ interface WeatherDataProps {
 }
 
 const DisplayWeather = () => {
-  const api_key = "5da2a1f6467eb16f2306e121d44fa5b9";
-  const api_Endpoint = "https://api.openweathermap.org/data/2.5/";
+  const api_key = process.env.REACT_APP_API_KEY;
+  const api_Endpoint = process.env.REACT_APP_API_ENDPOINT;
 
   const [weatherData, setWeatherData] = useState<WeatherDataProps | null>(null);
+  // 로딩 관련 아이콘 설정
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [searchCity, setSearchCity] = useState("");
 
   console.log(api_Endpoint);
   console.log(api_key);
@@ -46,47 +50,112 @@ const DisplayWeather = () => {
     return response.data;
   };
 
-  // const iconChanger = (weather: string) => {
-  //   let iconElement: ReactNode;
-  //   let iconColor: string;
+  const fetchWeatherData = async (city: string) => {
+    try {
+      const url = `${api_Endpoint}weather?q=${city}&appid=${api_key}&units=metric`;
+      const searchResponse = await axios.get(url);
 
-  //   switch (weather) {
-  //     case: "Rain"
-  //     iconElement = <BsFillCloudRainFill />
-  //     iconColor="#272829"
-  //   }
-  // }
+      const currentWeatherData: WeatherDataProps = searchResponse.data;
+      return { currentWeatherData };
+    } catch (error) {
+      console.error("No Data Found");
+      throw error;
+    }
+  };
+
+  const handleSearch = async () => {
+    if (searchCity.trim() === "") {
+      return;
+    }
+
+    try {
+      const { currentWeatherData } = await fetchWeatherData(searchCity);
+      setWeatherData(currentWeatherData);
+    } catch (error) {
+      console.error("No Results Found");
+    }
+  };
+
+  // Enter 키를 눌렀을 때 검색을 수행하는 함수
+  const handleKeyPress = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      await handleSearch();
+    }
+  };
+
+  const iconChanger = (weather: string) => {
+    let iconElement: ReactNode;
+    let iconColor: string;
+
+    switch (weather) {
+      case "Rain":
+        iconElement = <BsFillCloudRainFill />;
+        iconColor = "#272829";
+        break;
+      case "Clear":
+        iconElement = <BsFillSunFill />;
+        iconColor = "#FFC436";
+        break;
+      case "Clouds":
+        iconElement = <BsCloudyFill />;
+        iconColor = "#102C57";
+        break;
+      case "Mist":
+        iconElement = <BsCloudFog2Fill />;
+        iconColor = "#279EFF";
+        break;
+      default:
+        iconElement = <TiWeatherPartlySunny />;
+        iconColor = "#7B2B69";
+    }
+
+    return (
+      <span className="icon" style={{ color: iconColor }}>
+        {iconElement}
+      </span>
+    );
+  };
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const { latitude, longitude } = position.coords;
-      Promise.all([fetchCurrentWeather(latitude, longitude)]).then(
-        ([currentWeather]) => {
-          console.log(currentWeather);
-          setWeatherData(currentWeather);
-        }
-      );
-    });
-  }, []);
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const { latitude, longitude } = position.coords;
+        const [currentWeather] = await Promise.all([
+          fetchCurrentWeather(latitude, longitude),
+        ]);
+        setWeatherData(currentWeather);
+        setIsLoading(true);
+      });
+    };
 
+    fetchData();
+  }, []);
   return (
     <MainWrapper>
       <div className="container">
         <div className="searchArea">
-          <input type="text" placeholder="도시 이름을 입력해주세요" />
+          <input
+            type="text"
+            placeholder="도시 이름을 입력해주세요"
+            value={searchCity}
+            onChange={(e) => setSearchCity(e.target.value)}
+            onKeyDown={handleKeyPress}
+          />
 
           <div className="searchCircle">
-            <AiOutlineSearch className="searchIcon" />
+            <AiOutlineSearch className="searchIcon" onClick={handleSearch} />
           </div>
         </div>
 
-        {weatherData && (
+        {weatherData && isLoading ? (
           <>
             <div className="weatherArea">
               <h1>{weatherData.name}</h1>
               <span>{weatherData.sys.country}</span>
-              <div className="icon">icon</div>
-              <h1>{weatherData.main.temp}</h1>
+              <div className="icon">
+                {iconChanger(weatherData.weather[0].main)}
+              </div>
+              <h1>{weatherData.main.temp.toFixed(0)}</h1>
               <h2>{weatherData.weather[0].main}</h2>
             </div>
 
@@ -109,6 +178,11 @@ const DisplayWeather = () => {
               </div>
             </div>
           </>
+        ) : (
+          <div className="loading">
+            <RiLoaderFill className="loadingIcon" />
+            <p>loading</p>
+          </div>
         )}
       </div>
     </MainWrapper>
