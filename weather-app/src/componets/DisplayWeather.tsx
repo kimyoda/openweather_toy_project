eaeb5cdf67112bfd2e1c0db22b6a38ae8c2fd602
kimyoda <rladygks1210@naver.com>
2024-07-08 23:33:22 +0900

    [feat] Weatheropen Type of weather

--- weather-app/src/componets/DisplayWeather.tsx
+++ weather-app/src/componets/DisplayWeather.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { MainWrapper } from "./styles.module";
 import { AiOutlineSearch } from "react-icons/ai";
 import { WiHumidity } from "react-icons/wi";
@@ -13,9 +13,29 @@ import { RiLoaderFill } from "react-icons/ri";
 import { TiWeatherPartlySunny } from "react-icons/ti";
 import axios from "axios";
 
+interface WeatherDataProps {
+  name: string;
+
+  main: {
+    temp: number;
+    humidity: number;
+  };
+  sys: {
+    country: string;
+  };
+  weather: {
+    main: string;
+  }[];
+  wind: {
+    speed: number;
+  };
+}
+
 const DisplayWeather = () => {
   const api_key = "5da2a1f6467eb16f2306e121d44fa5b9";
   const api_Endpoint = "https://api.openweathermap.org/data/2.5/";
+
+  const [weatherData, setWeatherData] = useState<WeatherDataProps | null>(null);
 
   console.log(api_Endpoint);
   console.log(api_key);
@@ -26,16 +46,28 @@ const DisplayWeather = () => {
     return response.data;
   };
 
+  // const iconChanger = (weather: string) => {
+  //   let iconElement: ReactNode;
+  //   let iconColor: string;
+
+  //   switch (weather) {
+  //     case: "Rain"
+  //     iconElement = <BsFillCloudRainFill />
+  //     iconColor="#272829"
+  //   }
+  // }
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       const { latitude, longitude } = position.coords;
       Promise.all([fetchCurrentWeather(latitude, longitude)]).then(
         ([currentWeather]) => {
           console.log(currentWeather);
+          setWeatherData(currentWeather);
         }
       );
     });
-  });
+  }, []);
 
   return (
     <MainWrapper>
@@ -48,32 +80,36 @@ const DisplayWeather = () => {
           </div>
         </div>
 
-        <div className="weatherArea">
-          <h1>Auckland</h1>
-          <span>Nz</span>
-          <div className="icon">icon</div>
-          <h1>18c</h1>
-          <h2>cloudy</h2>
-        </div>
-
-        <div className="bottomInfoArea">
-          <div className="humidityLevel">
-            <WiHumidity className="windIcon" />
-            <div className="humidInfo">
-              <h1>60%</h1>
-              <p>Humidity</p>
+        {weatherData && (
+          <>
+            <div className="weatherArea">
+              <h1>{weatherData.name}</h1>
+              <span>{weatherData.sys.country}</span>
+              <div className="icon">icon</div>
+              <h1>{weatherData.main.temp}</h1>
+              <h2>{weatherData.weather[0].main}</h2>
             </div>
-          </div>
 
-          <div className="wind">
-            {/* 자료와 Wind icons이 다르기 때문에 주의해서 작성 */}
-            <FaWind className="windIcon" />
-            <div className="humidInfo">
-              <h1>2.35km/h</h1>
-              <p>wind speed</p>
+            <div className="bottomInfoArea">
+              <div className="humidityLevel">
+                <WiHumidity className="windIcon" />
+                <div className="humidInfo">
+                  <h1>{weatherData.main.humidity}%</h1>
+                  <p>Humidity</p>
+                </div>
+              </div>
+
+              <div className="wind">
+                {/* 자료와 Wind icons이 다르기 때문에 주의해서 작성 */}
+                <FaWind className="windIcon" />
+                <div className="humidInfo">
+                  <h1>{weatherData.wind.speed}km/h</h1>
+                  <p>wind speed</p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          </>
+        )}
       </div>
     </MainWrapper>
   );
